d5d2cc93bfa31b19bafe5a8bde544f1e968047bf
moka365 <yee23739565@gmail.com>
2023-02-22 15:43:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/firstbmi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.firstbmi;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.res.Configuration
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
